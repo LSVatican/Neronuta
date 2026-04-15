46897fadffdd15ea6b8ef366e7e2fd090f63b560
LSVatican <malikalfirdausyogi@gmail.com>
2026-04-15 21:52:59 +0700

    update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const REDIRECT_URI = encodeURIComponent('https://neronuta.vercel.app/home.html')
 
 function loginDiscord() {
     const scope = 'identify';
-    const authUrl = `https://discord.com/oauth2/authorize?client_id=1493975600870850600&response_type=code&redirect_uri=https%3A%2F%2Fneronuta.vercel.app%2F&scope=identify`;
+    const authUrl = `https://discord.com/oauth2/authorize?client_id=1493975600870850600&response_type=code&redirect_uri=https%3A%2F%2Fneronuta.vercel.app%2Fhome.html&scope=identify`;
     
     window.location.href = authUrl;
 }
